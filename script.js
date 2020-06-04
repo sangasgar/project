d86255d845b76321cad0163cd5a98db29165a13d
Garegin <sangas@yandex.ru>
2020-06-05 01:41:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+﻿"use strict";
+a = 6;
+console.log(a)
+var leftBorderWidth = 1;
+{
+    let second = 2;  
+}
+
+
+
+var sym = Symbol();
+console.log(sym)
+
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+console.log(persone["name"]);
+
+
+let arr = ["plum.png","orange.jpg","apple.bmp"];
+
+console.log(arr[2])
+//  alert("Hello world");
+
+//  let answer = confirm("Are you here");
+//  console.log(answer);
+
+// let answer = +prompt("Есть ли Вам 18 лет?", "Да");
+// console.log(typeof(answer))
+
+// console.log("arr" + +"object")
+
+let incr = 10,
+    decr = 10;
+
+
+
+    console.log(incr++);
+    console.log(decr--);
+//console.log(5%2);
+//console.log("2" === 2);
+
+
+let  isChecked = false;
+let  isClose = false;
+
+console.log(isChecked || !isClose);
